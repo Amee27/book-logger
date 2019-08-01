8e5c6dd821ce5fb324400145dce46e6126ec6bb8
Amita Nakrani <amita.nakrani@gmail.com>
2019-07-31 23:32:44 -0500

    Initial commit

---
+++ src/main/java/com/amee/booklogger/BookLoggerApplication.java
@@ -0,0 +1,13 @@
+package com.amee.booklogger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookLoggerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookLoggerApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/amee/booklogger/controllers/LoginController.java
@@ -0,0 +1,15 @@
+package com.amee.booklogger.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class LoginController {
+
+    @RequestMapping(value = "")
+    @ResponseBody
+    public String index() {
+        return "Hello World";
+    }
+}
