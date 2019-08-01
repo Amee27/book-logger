9d3dca5343b8daf085115bd113843a74de37a085
Amita Nakrani <amita.nakrani@gmail.com>
2019-07-31 23:46:12 -0500

    Initial commit

--- src/main/java/com/amee/booklogger/controllers/LoginController.java
+++ src/main/java/com/amee/booklogger/controllers/LoginController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class LoginController {
 
+
     @RequestMapping(value = "")
     @ResponseBody
     public String index() {
